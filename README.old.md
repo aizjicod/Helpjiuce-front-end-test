89876378cd7481d0c2c921569197ec9f0df358d8
Aiziji <aletv1501@gmail.com>
2022-12-04 18:36:24 -0400

    Set up basic sttructure of project

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# Helpjiuce-front-end-test
+this is a take home test for front end in helpjiuce
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Main from "./components/main";
+
+function App() {
+  return (
+    <div className="App container-flex">
+      <header className="App-header container-flex">
+        <Main />
+      </header>
+    </div>
+  );
+}
+
+export default App;

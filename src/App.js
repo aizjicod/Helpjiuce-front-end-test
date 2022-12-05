f63132c740ea04aefd0a672cab1173b7f6673bf6
Aiziji <aletv1501@gmail.com>
2022-12-05 16:11:36 -0400

    Add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Main from './components/main';
 import TextEditor from './components/text-editor';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App container-flex">
       <header className="App-header container-flex">
+        <Header />
         <Main />
       </header>
       <TextEditor />
